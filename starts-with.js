719727efb65d346d5ae866f3f6d5b6d12e702ced
Anil Kumar <anilkoundal00@gmail.com>
2022-05-27 22:44:58 +0530

    Update starts-with.js

--- starts-with.js
+++ starts-with.js
@@ -1,16 +1,13 @@
 
-<script>
 // to check string index. indexOf is case senstive.
 let stri = "good after noon";
 let strpos = stri.indexOf("good");
 if(strpos===0){
   alert('done');
 }
-</script>
 
-<script>
-  // to check string starts with.
+// to check string starts with.
 let text = "hello user, welcome to the git.";
 let result = text.startsWith("Hello");
 alert(result);
-</script>
+
